7f5dfcfc7bed617f8e2811b37c8725a94d1b3a2b
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2017-10-11 13:47:03 +0200

    Add files via upload

--- Memory2/Form1.cs
+++ Memory2/Form1.cs
@@ -16,6 +16,10 @@ namespace Memory2
         int Rows = 4;
         int Columns = 4;
         PictureBox[] Plaatjes;
+
+        //Label firstClicked = null;
+        //Label secondClicked = null;
+
         public Form1()
         {
             InitializeComponent();
@@ -31,10 +35,34 @@ namespace Memory2
         //Genereert kaarten in random volgorde
         private void randomAanmaken()
         {
+            //randomiser
+            //Random Randomizer = new Random();
+            //int imageplacer = Randomizer.Next(1, 16);
 
-            Random Randomizer = new Random();
-            int imageplacer = Randomizer.Next(1, 16);
+            //shuffle
+                int[] shuf = new int[16] { 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8};
+                Random rnd = new Random();
+            /*
+                for (int t = 0; t < shuf.Length; t++)
+                {
+                    int tmp = shuf[t];
+                    int r = rnd.Next(t, shuf.Length);
+                    shuf[t] = shuf[r];
+                    shuf[r] = tmp;
+                }
+            */
+
+            //images array size
             int[] ImagesArray = new int[Rows * Columns];
+
+            //tags invullen array, 0 = niet gedraaid, 1 = gedraaid, 2 = geraden
+            int[] TagArray = new int[Rows * Columns];
+            for (int j = 0; j <(Rows * Columns); j++)
+            {
+                TagArray[j] = 0;
+            }
+
+            //plaatjes raster loops
             Plaatjes = new PictureBox[Rows * Columns];
             int i = 0;
             for (int cRow = 0; cRow < Rows; cRow++)
@@ -47,13 +75,25 @@ namespace Memory2
                     string imgpath = (Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName).ToString() + @"\placeholder\kaartje0" + ".png";
                     Box.Image = System.Drawing.Image.FromFile(imgpath);
                     Box.Location = new System.Drawing.Point(10 + cColumn * 210, cRow * 210);
-                    Box.Name = "" + i;
-                    if (i >8)
+
+
+                    //randomiser
+                    int tmp = shuf[i];
+                    int r = rnd.Next(i, shuf.Length);
+                    shuf[i] = shuf[r];
+                    shuf[r] = tmp;
+
+                    Box.Name = "" + shuf[r];
+                    int halfway = (Rows * Columns) / 2;
+                    if (shuf[r] > halfway)
                     {
-                        Box.Tag = i - 8;
+                        Box.Tag = shuf[r] - halfway;
                     }
-                    else {
-                        Box.Tag = i; }
+                    else
+                    {
+                        Box.Tag = shuf[r];
+                    }
+
                     Box.Size = new System.Drawing.Size(128, 128);
                     Plaatjes[i] = Box;
                     i++;
@@ -75,10 +115,13 @@ private void InitializeComponent()
         public void Box_Click(object sender, EventArgs e)
         {
             PictureBox Boxje = (PictureBox)sender;
-            
+
+            //Label clickedLabel = sender as Label;
+
             string imgpath = (Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName).ToString() + @"\placeholder\kaartje" + Boxje.Name + ".png";
             Boxje.Image = System.Drawing.Image.FromFile(imgpath);
 
+            /*
             if (Boxje.Tag != null)
             {
                 if (Boxje.BackColor ==Color.Black)
@@ -86,6 +129,15 @@ private void InitializeComponent()
                 Boxje.BackColor = Color.Black;
             }
 
+            secondClicked = clickedLabel;
+            secondClicked.ForeColor = Color.Black;
+            if (firstClicked.Text == secondClicked.Text)
+            {
+                firstClicked = null;
+                secondClicked = null;
+                return;
+            }
+            */
 
             /*
             secondClicked = Boxje.Tag;
