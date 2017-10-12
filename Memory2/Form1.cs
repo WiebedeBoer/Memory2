7ff44f6d8cb359d02c4abad47e0fbe0932059987
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2017-10-12 13:43:20 +0200

    Add files via upload

--- Memory2/Form1.cs
+++ Memory2/Form1.cs
@@ -16,6 +16,8 @@ namespace Memory2
         int Rows = 4;
         int Columns = 4;
         PictureBox[] Plaatjes;
+        int[] TagArray;
+
 
         //Label firstClicked = null;
         //Label secondClicked = null;
@@ -35,28 +37,30 @@ namespace Memory2
         //Genereert kaarten in random volgorde
         private void randomAanmaken()
         {
-            //randomiser
-            //Random Randomizer = new Random();
-            //int imageplacer = Randomizer.Next(1, 16);
+            //half of total cards
+            int halfway = (Rows * Columns) / 2;
 
-            //shuffle
-                int[] shuf = new int[16] { 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8};
-                Random rnd = new Random();
-            /*
-                for (int t = 0; t < shuf.Length; t++)
+            //shuffle array declareren
+            int[] shuf = new int[Rows * Columns];
+            Random rnd = new Random();
+            //shuffle array vullen
+            for (int k = 0; k < (Rows * Columns); k++)
+            {
+                if (k > halfway)
                 {
-                    int tmp = shuf[t];
-                    int r = rnd.Next(t, shuf.Length);
-                    shuf[t] = shuf[r];
-                    shuf[r] = tmp;
+                    shuf[k] = k + 1 - halfway;
                 }
-            */
+                else
+                {
+                    shuf[k] = k + 1;
+                }
+            }
 
             //images array size
             int[] ImagesArray = new int[Rows * Columns];
 
             //tags invullen array, 0 = niet gedraaid, 1 = gedraaid, 2 = geraden
-            int[] TagArray = new int[Rows * Columns];
+            TagArray = new int[Rows * Columns];
             for (int j = 0; j <(Rows * Columns); j++)
             {
                 TagArray[j] = 0;
@@ -76,15 +80,14 @@ namespace Memory2
                     Box.Image = System.Drawing.Image.FromFile(imgpath);
                     //locatie van box
                     Box.Location = new System.Drawing.Point(10 + cColumn * 210, cRow * 210);
-                    
                     //randomiser
                     int tmp = shuf[i];
                     int r = rnd.Next(i, shuf.Length);
                     shuf[i] = shuf[r];
                     shuf[r] = tmp;
-
+                    //box naam
                     Box.Name = "" + shuf[i];
-                    int halfway = (Rows * Columns) / 2;
+                    //box tag
                     if (shuf[i] > halfway)
                     {
                         Box.Tag = shuf[i] - halfway;
@@ -98,9 +101,11 @@ namespace Memory2
                     Plaatjes[i] = Box;
                     //increment voor random
                     i++;
+                    //box toevoegen
                     this.Controls.Add(Box);
                     //clicker
                     Box.Click += Box_Click;
+                    //layout kaartjes
                     ((System.ComponentModel.ISupportInitialize)(Box)).EndInit();
                     this.ResumeLayout(false);
                 }
@@ -108,21 +113,66 @@ namespace Memory2
         }
 
         /*
-private void InitializeComponent()
-{
-    throw new NotImplementedException();
-}
-*/
+        private void InitializeComponent()
+        {
+        throw new NotImplementedException();
+        }
+        */
 
         public void Box_Click(object sender, EventArgs e)
         {
+            //event koppelen aan box
             PictureBox Boxje = (PictureBox)sender;
 
+            //string imgpath = (Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName).ToString() + @"\placeholder\kaartje" + Boxje.Tag + ".png";
+            //Boxje.Image = System.Drawing.Image.FromFile(imgpath);
+
+            //plaats zoeken in array         
+            int ClickedNum = Convert.ToInt32(Boxje.Name) - 1;
+            //image path en tag veranderen, bij wel draaien, check of het 0, of 1 of 2 is
+            if (TagArray[ClickedNum] == 0)
+            {
+                //draai kaartje om
+                string imgpath = (Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName).ToString() + @"\placeholder\kaartje" + Boxje.Tag + ".png";
+                Boxje.Image = System.Drawing.Image.FromFile(imgpath);
+
+                //zetten
+                int FirstClicked = 0;
+                int SecondClicked = 0;
+                //spoor op in array of er een eerder geklikt kaartje is
+                for (int l = 0; l <(Rows * Columns); l++)
+                {
+                    if (TagArray[l] ==1)
+                    {
+                        FirstClicked = l;
+                    } 
+                }
+                //als er al wel een kaart is omgedraaid in een beurt, oftewel tweede zet in beurt van een speler
+                if (FirstClicked > 0)
+                {
+                    /*
+                    //als het matched
+                    if (Boxje.Tag[FirstClicked] == Boxje.Tag[ClickedNum])
+                    {
+                        FirstClicked = FirstClicked;
+                        SecondClicked = ClickedNum;
+
+                        TagArray[SecondClicked] = 2;
+                        TagArray[FirstClicked] = 2;
+                    }
+                    */
+
+                }
+                //als er nog geen kaart is omgedraaid, oftewel eerste zet in beurt van een speler
+                else
+                {
+                    //als het matched
+                    TagArray[ClickedNum] = 1;
+                }
+            }
+            
+
             //Label clickedLabel = sender as Label;
-
-            string imgpath = (Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName).ToString() + @"\placeholder\kaartje" + Boxje.Name + ".png";
-            Boxje.Image = System.Drawing.Image.FromFile(imgpath);
-
             /*
             if (Boxje.Tag != null)
             {
@@ -151,8 +201,8 @@ private void InitializeComponent()
                 return;
             }
             */
-
         }
+
         private int IndexPlaatjes(PictureBox Boks)
         {
             int i = 0;
