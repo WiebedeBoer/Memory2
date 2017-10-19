709a3189f2a7797929c08ad2c2f191a33010637b
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2017-10-19 11:40:06 +0200

    Add files via upload

--- Memory2/hoofdmenu.Designer.cs
+++ Memory2/hoofdmenu.Designer.cs
@@ -36,7 +36,7 @@
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(76, 47);
-            this.button1.Name = "button1";
+            this.button1.Name = "Form1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "New Game";
@@ -46,12 +46,12 @@
             // button2
             // 
             this.button2.Location = new System.Drawing.Point(76, 127);
-            this.button2.Name = "button2";
+            this.button2.Name = "Highscores";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
             this.button2.Text = "Highscores";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.button1_Click);
             // 
             // button3
             // 
@@ -61,7 +61,7 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "Load Game";
             this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.Click += new System.EventHandler(this.button1_Click);
             // 
             // hoofdmenu
             // 
--- Memory2/hoofdmenu.cs
+++ Memory2/hoofdmenu.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,22 +13,73 @@ namespace Memory2
 {
     public partial class hoofdmenu : Form
     {
+        Thread th;
+
         public hoofdmenu()
         {
             InitializeComponent();
         }
 
         private void button1_Click(object sender, EventArgs e)
-        {
-            var myForm = new Form1();
-            myForm.Show();
+        {            
+            var formButton = (Control)sender;
+            string formName = formButton.Name;
+            if (formName =="Form1")
+            {
+                this.Close();
+                th = new Thread(opennewform);
+                th.SetApartmentState(ApartmentState.STA);
+                th.Start();
+                //Form myForm = new Form1();
+                //myForm.Show();
+            }
+            else if (formName =="Highscores") {
+                this.Close();
+                th = new Thread(opennewhigh);
+                th.SetApartmentState(ApartmentState.STA);
+                th.Start();
+                //Form myForm = new highscores();
+                //myForm.Show();
+            }
+            else if (formName == "Opties")
+            {
+                this.Close();
+                th = new Thread(opennewopties);
+                th.SetApartmentState(ApartmentState.STA);
+                th.Start();
+                //Form myForm = new highscores();
+                //myForm.Show();
+            }
+            else {
+            }
+
+            
         }
 
+        private void opennewform(object obj)
+        {
+            //throw new NotImplementedException();
+            Application.Run(new Form1());
+        }
+
+        private void opennewhigh(object obj)
+        {
+            //throw new NotImplementedException();
+            Application.Run(new highscores());
+        }
+
+        private void opennewopties(object obj)
+        {
+            //throw new NotImplementedException();
+            Application.Run(new opties());
+        }
+
+        /*
         private void button2_Click(object sender, EventArgs e)
         {
-            var myForm = new highscores();
-            myForm.Show();
+           
         }
+        */
 
         private void button3_Click(object sender, EventArgs e)
         {
