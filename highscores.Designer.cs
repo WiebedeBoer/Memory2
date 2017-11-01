ff244b38b182261616aaae23f0b951c1b76ee3f9
IdsVeen <32294497+IdsVeen@users.noreply.github.com>
2017-11-01 13:26:12 +0100

    Add files via upload

---
+++ highscores.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Memory2
+{
+    partial class highscores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        /// 
+
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveBorder;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 65);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 20;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView1.Size = new System.Drawing.Size(392, 177);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // listView1
+            // 
+            this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.listView1.GridLines = true;
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listView1.Location = new System.Drawing.Point(531, 157);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(121, 97);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // highscores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(184, 462);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "highscores";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
+    /*
+    private void InitializeComponent()
+    {
+        this.components = new System.ComponentModel.Container();
+        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        this.Text = "highscores";
+
+        // 
+        // Form1
+        // 
+        this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        this.ClientSize = new System.Drawing.Size(984, 462);
+        this.Name = "Form1";
+        this.Text = "Form1";
+        this.Load += new System.EventHandler(this.Highscores_Load);
+        this.ResumeLayout(false);
+    }
+    */
+/*
+#endregion
+}
+}
+*/
---
+++ highscores.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+
+namespace Memory2
+{
+    public partial class highscores : Form
+    {
+        public highscores()
+        {
+            InitializeComponent();
+
+            int rankcounter = 1;
+            string rankdisplay = Convert.ToString(rankcounter);
+            if (File.Exists("data.xml"))
+            {
+                DataSet ds = new DataSet();
+                ds.ReadXml("data.xml", XmlReadMode.InferTypedSchema);
+                ds.Tables[0].DefaultView.Sort = "Score DESC";
+                dataGridView1.DataSource = ds.Tables[0];
+                dataGridView1.Location = new Point(30, 0);
+                dataGridView1.Size = new Size(150, (20 * (dataGridView1.RowCount+ 1)) + 3);
+                
+                listView1.Location = new Point(0, 0);
+                listView1.Size = new Size(25, (20 * (dataGridView1.RowCount + 1)) + 3);
+                listView1.Columns.Add("#");
+                listView1.View = View.Details;
+                listView1.Scrollable = false;
+                
+
+                for (int rowc = 0; rowc < dataGridView1.Rows.Count; rowc++)
+                {
+                    listView1.Items.Add(new ListViewItem(rankdisplay));
+                    rankcounter++;
+                    rankdisplay = Convert.ToString(rankcounter);
+                }
+                
+            }
+
+            /*
+            private void button1_Click(object sender, EventArgs e)
+            {
+                try
+                {
+                    Scores info = new Scores();
+                    info.Naam = textBoxNaam.Text;
+                    info.Score = int.Parse(textBoxScore.Text);
+                    AppendData(info, "data.xml");
+                    MessageBox.Show("De score is toegevoegd");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            */
+
+            /*
+            static void AppendData(Scores obj, string filename)
+            {
+                XmlSerializer xmlser = new XmlSerializer(typeof(List<Scores>));
+                List<Scores> list = null;
+                try
+                {
+                    using (Stream s = File.OpenRead(filename))
+                    {
+                        list = xmlser.Deserialize(s) as List<Scores>;
+                    }
+                }
+                catch
+                {
+                    list = new List<Scores>();
+                }
+                list.Add(obj);
+                using (Stream s = File.OpenWrite(filename))
+                {
+                    xmlser.Serialize(s, list);
+                }
+            }
+            */
+
+        }
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*
+
+        string[][] highscoresarr1 = new string[][] { new string[2] { "6", "Na1" }, new string[2] { "2", "Na2" }, new string[2] { "3", "Na3" }, new string[2] { "5", "Na4" } };
+
+        private void highscoresTool()
+        {
+            Panel dynamicPanel = new Panel();
+            dynamicPanel.Location = new Point(0, 0);
+            dynamicPanel.Name = "Panel1";
+            dynamicPanel.Size = new Size(400, 400);
+            dynamicPanel.BackColor = Color.LightBlue;
+
+            ListView rank = new ListView();
+            rank.Location = new Point(10, 30);
+            rank.Size = new Size(25, 100);
+            rank.Columns.Add("#", 100);
+            rank.View = View.Details;
+            rank.Scrollable = false;
+
+            ListView highscores = new ListView();
+            highscores.Location = new Point(50, 30);
+            highscores.Size = new Size(105, 100);
+            highscores.Columns.Add("Points", 50);
+            highscores.Columns.Add("Name", 50);
+            highscores.View = View.Details;
+
+            int i = 0;
+
+            foreach (string[] subArray in highscoresarr1)
+            {
+                string j = Convert.ToString(i + 1);
+                ListViewItem item1 = new ListViewItem(highscoresarr1[i][0], 0);
+                item1.SubItems.Add(highscoresarr1[i][1]);
+                highscores.Items.AddRange(new ListViewItem[] { item1 });
+                i++;
+                rank.Items.Add(new ListViewItem(j));
+            }
+
+            highscores.Sorting = SortOrder.Descending;
+
+            dynamicPanel.Controls.Add(rank);
+            dynamicPanel.Controls.Add(highscores);
+            Controls.Add(dynamicPanel);
+
+        }
+        */
+
+        /*
+        //onload
+        private void Highscores_Load(object sender, EventArgs e)
+        {
+            //highscores
+            highscoresTool();
+
+
+        }
+            */
+
+    }
+}
+
